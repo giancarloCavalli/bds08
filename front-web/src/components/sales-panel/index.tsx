356e1d4428b763396763c600502dd1695e8f5e4c
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-17 14:06:04 -0300

    Donut Chart

--- front-web/src/components/sales-panel/index.tsx
+++ front-web/src/components/sales-panel/index.tsx
@@ -1,11 +1,24 @@
+import ReactApexChart from 'react-apexcharts';
+import { buildPieChartConfig } from './helpers';
 import { SalesSumValue } from './sales-sum-value';
 import './styles.css';
+
+const labels = ['Feminino', 'Masculino', 'Outro'];
+const series = [44, 55, 13];
 
 export const SalesPanel = () => {
   return (
     <div className="sales-panel-container base-card">
       <SalesSumValue />
-      <div></div>
+      <div className="sales-panel-donut-container">
+        <ReactApexChart
+          options={buildPieChartConfig(labels, 'Lojas')}
+          type="donut"
+          width={270}
+          height={420}
+          series={series}
+        />
+      </div>
     </div>
   );
 };
