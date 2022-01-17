d5a787421285d71a8635066be0bcde62678aaaf0
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-17 10:52:23 -0300

    eslint prettier, global styles

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
+      <h1>Herou Warudo</h1>
     </div>
   );
 }
