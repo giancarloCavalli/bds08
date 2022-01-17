47604f56940e0bb1ec80ed91d627afb91faf824d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-17 14:34:49 -0300

    Fetching stores in Filter component

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,18 +1,43 @@
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { requestBackend } from '../../utils/requests';
+import { Store } from '../../types/types';
 
 type Props = {
   className?: string;
 };
 
 export const Filter = ({ className }: Props) => {
+  const [stores, setStores] = useState<Store[]>([]);
+
+  useEffect(() => {
+    let isApiSubscribed = true;
+
+    requestBackend
+      .get<Store[]>('/stores')
+      .then((response) => {
+        if (isApiSubscribed) {
+          setStores(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log('Erro GET stores', error);
+      });
+
+    return () => {
+      isApiSubscribed = false;
+    };
+  }, []);
+
   return (
     <div className={`filter-container base-card ${className}`}>
       <select name="lojas">
         <option value="">Seleciona uma loja</option>
-        <option value="Indaial">Indaial</option>
-        <option value="Timbó">Timbó</option>
-        <option value="Blumenau">Blumenau</option>
-        <option value="Pomerode">Pomerode</option>
+        {stores.map((store) => (
+          <option key={store.id} value={store.name}>
+            {store.name}
+          </option>
+        ))}
       </select>
     </div>
   );
